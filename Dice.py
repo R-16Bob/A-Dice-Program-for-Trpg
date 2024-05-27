e9b87bc36cdef727e8e880eb25bbf1079f75b770
R-16Bob <15343443062@163.com>
2024-05-27 22:44:21 +0800

    add argparse and edit README

--- Dice.py
+++ Dice.py
@@ -1,73 +1,78 @@
-#这是一个骰子程序     
-#可以输入骰子面数和次数。    by R-16Bob
-#  更新v2.01 增加命令功能,修改D100有0的错误
-# v2.02 增加提示符
+# 这是一个骰子程序
+# 可以输入骰子面数和次数。    by R-16Bob
+# V3.0 现在可以直接在命令行选择功能了
 import time
 import random
+import argparse
 
-def help():
-	print("---------------------------------------------------") 
-	print("t- roll a D100")
-	print("m- jump to main program")
-	print("h- You now are using this function")
-	print("d- 安科模式")
-	print("e- exit this program")
-	
 def ten():
-	print("Rolling...")	
-	time.sleep(1)
-	d100=random.randint(1,100)
-	print(d100)
-	
+    again = 'y'
+    while(1):
+        print("Rolling...")
+        time.sleep(1)
+        d100 = random.randint(1, 100)
+        print(d100)
+        print("roll again?(y or n)")
+        again = input(prompt)
+        if again!='y':
+            break
+
+
 def main():
-	again='y'
-	while(again=='y'):
-		print("Enter number of faces:")
-		face=int(input(prompt))                      ##输入骰子面数
-		print("Enter number of Dice:")
-		number=int(input(prompt))                   ##输入骰子个数
-		print("------------------------------------------------")
-		for i in range(number):
-			print("Rolling...")
-			time.sleep(1)
-			result=random.randint(1,face)
-			print(result)
-		print("roll again?(y or n)")
-		again=input(prompt)
+    again = 'y'
+    while (again == 'y'):
+        print("Enter number of faces:")
+        face = int(input(prompt))  ##输入骰子面数
+        print("Enter number of Dice:")
+        number = int(input(prompt))  ##输入骰子个数
+        print("------------------------------------------------")
+        for i in range(number):
+            print("Rolling...")
+            time.sleep(1)
+            result = random.randint(1, face)
+            print(result)
+        print("roll again?(y or n)")
+        again = input(prompt)
 
-def d():
-	while(True):
-		str=input("input: ")
-		num=int(str[0])
-		face=int(str[2:])
-		if num==1:
-			print(f'【{str}={random.randint(1,face)}】')
-		else:
-			print(f'【{str}=',end='')
-			print(f'{random.randint(1, face)}',end='')
-			for i in range(num-1):
-				print(', ', end='')
-				print(f'{random.randint(1,face)}',end='')
-			print('】')
 
+def anko():
+    while (True):
+        str = input("input: ")
+        if str == 'q':
+            break
+        num = int(str[0])
+        face = int(str[2:])
+
+        if num == 1:
+            print(f'【{str}={random.randint(1, face)}】')
+        else:
+            print(f'【{str}=', end='')
+            print(f'{random.randint(1, face)}', end='')
+            for i in range(num - 1):
+                print(', ', end='')
+                print(f'{random.randint(1, face)}', end='')
+            print('】')
 
 
 def menu():
-	print("This is a Dice program by R-16Bob")
-	print("Enter 'h'for help if you first use .")
-	while(1):
-		command=input(prompt)
-		if(command=='h'):
-			help()
-		if(command=='t'):
-			ten()
-		if(command=='m'):
-			main()
-		if(command=='d'):
-			d()
-		if(command=='e'):
-			break 
-		
-			
-prompt = ">>"
-menu() 			
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--Roll_a_D100', action='store_true', default=False)
+    parser.add_argument('-r', '--Roll_a_Dice', action='store_true', default=True, help='Roll a Dice')
+    parser.add_argument('-a', '--Anko', action='store_true', default=False, help='Anko mode')
+
+    args = parser.parse_args()  # 解析命令行参数到args变量中
+
+    if args.Roll_a_D100:
+        ten()
+    elif args.Anko:
+        print('input a string, for example,1d6. type q to quit')
+        anko()
+    elif args.Roll_a_Dice:
+        main()
+    else:
+        parser.print_help()  # 如果没有匹配的参数，则打印帮助信息
+
+
+if __name__ == "__main__":
+    prompt = ">>"
+    menu()
